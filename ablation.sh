166e64ee98cc4ecc6d1630ff69e234c02882ef51
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-09-28 23:32:39 +1000

    bug fix

--- ablation.sh
+++ ablation.sh
@@ -5,4 +5,4 @@ set -x
 bash train_vit.sh dinov2_vitb_exp 1 False False False xe_only
 bash train_vit.sh dinov2_vits_exp 1 True False False clst_sep
 bash train_vit.sh dinov2_vits_exp 1 True True False clst_sep_orth
-bash train_vit.sh dinov2_vits_exp 1 True False True all
+bash train_vit.sh dinov2_vits_exp 1 True True True all
