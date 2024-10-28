97f4cf27db0228076eb09bd32efd0845a94d3431
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-10-29 00:43:21 +1100

    update

--- train.sh
+++ train.sh
@@ -5,13 +5,17 @@ set -x
 bash scripts/train.sh dinov2_vitb_exp 1 600
 bash scripts/train.sh dinov2_vitb_exp 1 1000
 bash scripts/train.sh dinov2_vitb_exp 1 2000
+
+bash scripts/train.sh dino_vitb8 1 2000
+bash scripts/train.sh dino_vits8 1 2000
+
 bash scripts/train.sh dinov2_vits_exp 1 600
 bash scripts/train.sh dinov2_vits_exp 1 1000
 bash scripts/train.sh dinov2_vits_exp 1 2000
 
-bash scripts/train.sh dino_vitb8 1 600
-bash scripts/train.sh dino_vitb8 1 1000
-bash scripts/train.sh dino_vitb8 1 2000
-bash scripts/train.sh dino_vits8 1 600
-bash scripts/train.sh dino_vits8 1 1000
-bash scripts/train.sh dino_vits8 1 2000
+# bash scripts/train.sh dino_vitb8 1 600
+# bash scripts/train.sh dino_vitb8 1 1000
+# bash scripts/train.sh dino_vitb8 1 2000
+# bash scripts/train.sh dino_vits8 1 600
+# bash scripts/train.sh dino_vits8 1 1000
+# bash scripts/train.sh dino_vits8 1 2000
