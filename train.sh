2cb143beec79813968b4fded402a3dfa1253aaea
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-10-31 00:49:50 +1100

    update model to run

--- train.sh
+++ train.sh
@@ -13,8 +13,8 @@ set -x
 # bash scripts/train.sh dinov2_vits_exp 1 1000
 # bash scripts/train.sh dinov2_vits_exp 1 2000
 
-bash scripts/train.sh dino_vits8 1 1000
-bash scripts/train.sh dino_vits8 1 600
+bash scripts/train.sh dino_vitb16 1 1000
+bash scripts/train.sh dino_vitb16 1 600
 
 # bash scripts/train.sh dino_vitb8 1 600
 # bash scripts/train.sh dino_vitb8 1 1000
