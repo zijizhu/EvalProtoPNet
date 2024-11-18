b8708d559b127fd268c642f8fe0b96da1b1baa30
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-11-18 13:24:53 +1100

    update training script

--- train.sh
+++ train.sh
@@ -2,6 +2,11 @@
 
 set -x
 
+bash train_cars.sh dinov2_vitb_exp 1 980
+bash train_cars.sh dinov2_vits_exp 1 980
+bash train_cars.sh dinov2_vitb_exp 1 588
+bash train_cars.sh dinov2_vits_exp 1 588
+
 # bash scripts/train.sh dinov2_vitb_exp 1 600
 # bash scripts/train.sh dinov2_vitb_exp 1 1000
 # bash scripts/train.sh dinov2_vitb_exp 1 2000
@@ -13,9 +18,9 @@ set -x
 # bash scripts/train.sh dinov2_vits_exp 1 1000
 # bash scripts/train.sh dinov2_vits_exp 1 2000
 
-bash scripts/train.sh dino_vitb16 1 2000
-bash scripts/train.sh dino_vitb16 1 1000
-bash scripts/train.sh dino_vitb16 1 600
+# bash scripts/train.sh dino_vitb16 1 2000
+# bash scripts/train.sh dino_vitb16 1 1000
+# bash scripts/train.sh dino_vitb16 1 600
 
 # bash scripts/train.sh dino_vitb8 1 600
 # bash scripts/train.sh dino_vitb8 1 1000
